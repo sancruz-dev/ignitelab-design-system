5a996b6f20d9399a8fa7df4fdc296eeb7d4d5828
Sancruz-dev <sancruz.dev@gmail.com>
2022-10-14 22:45:16 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className='text-2xl font-bold'>Hello World!</h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
+        Botão
+      </button>
+    </>
+  )
+}
+
